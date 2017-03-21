0f3db3e86019a557a5e93ccd82f2071cca296e5e
Bartłomiej <blysk88@gmail.com>
2017-03-21 11:22:28 +0100

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Start {
+
+	public static void main(String[] args) {
+
+		Var.scanner = new Scanner(System.in);
+		Menu.room_Start1();
+		// Room.room23(); //do testowania
+
+		}
+
+	}
+
+
---
+++ Var.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Var {
+	public static String choice = "xxx";
+	public static int luck = 0;
+	public static Scanner scanner;
+	public static String line = "";
+
+	public static boolean event1 = false; // true ranny
+	public static boolean event2 = false; // noz kuchenny
+	public static boolean event3 = false; // true = pawe
+	public static boolean event4 = false; // oczyszczona rana
+	public static boolean event5 = false; // martwa reka
+	public static boolean event6 = false; // uratowany naukowiec
+	public static boolean event7 = false; // rewolwer
+
+	public static String row = "========================================================================";
+	public static String read14 = "\n\n\n\n\n\n\n\n\n\n\n\n";
+
+}
